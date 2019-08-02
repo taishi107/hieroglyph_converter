1803ba1c80192161e36cfbe39239ea3b5d73d32f
taishi <uroko100manen@gmail.com>
2019-08-02 18:25:37 +0900

    Fixed bug

--- csv_convert.py
+++ csv_convert.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-data = pd.read_csv("data/unicode.txt",delimiter="|",header=None,names={"A","B","C"})
+data = pd.read_csv("data/unicode.txt",delimiter="|",names=["A","B","C"])
 str = pd.read_csv("data/input.txt",header=None)
-text = str.iloc[1,0]
-print(data['B'])
-
 
 print(str)
 for i in range(len(str)):
     text = str.iloc[i,0]
     output_str = ""
     for j in range(len(text)):
-        target_str = 'B == ' + '\"' + text[j] + '\"'
+        target_str = 'A == ' + '\"' + text[j] + '\"'
         num = data.reset_index().query(target_str).index[0]
         #print(data.iloc[num,0],data.iloc[num,1])
         output_str += data.iloc[num,1]
